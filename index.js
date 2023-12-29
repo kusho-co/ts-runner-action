34a4c374ce3c2c8bdc1ec722c65a6e7bb912086a
Sourabh Gawande <sourabh@kusho.co>
2023-12-29 16:08:54 +0530

    x

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ try {
   let testSuites = core.getInput("test-suites")
   testSuites = testSuites.split(",")
   for (ts of testSuites) {
-    console.log(`Running test suite ${ts}`);
-    core.setFailed(`2 test cases failed in ${ts}`)
+    core.setCommandEcho(`Running test suite ${ts}`);
+    core.error(`2 test cases failed in ${ts}`)
+    core.startGroup('More details')
+    core.setCommandEcho("TEST CASE -> Test with wrong HTTP method")
+    core.setCommandEcho("STATUS -> Failed")
+    core.setCommandEcho("DETAILS -> Expected an error response for wrong HTTP method. Got 200 response instead.")
+    core.endGroup()
   }
+  core.setFailed("Some test suites have failed. Check the logs for more details.")
 } catch (error) {
   core.setFailed(error.message);
 }
